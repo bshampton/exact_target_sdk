5e6c72e5b5c7ac29d81b3501e60d42726f4a0ea9
Wil Chung <iamwil@gmail.com>
2013-05-29 15:28:49 -0700

    added import definition

---
+++ lib/exact_target_sdk/import_definition.rb
@@ -0,0 +1,31 @@
+module ExactTargetSDK
+  class ImportDefinition < APIObject
+
+    property "AllowErrors"
+    property "Client"
+    property "ControlColumn"
+    int_property "CorrelationID"
+    property "CreatedDate"
+    property "CustomerKey"
+    property "Delimiter"
+    property "Description"
+    property "EndOfLineRepresentation"
+    property "FileSpec"
+    property "Filter"
+    int_property "HeaderLines"
+    int_property "ID"
+    property "InteractionObjectID"
+    property "Keyword"
+    property "MaxFileAge"
+    property "MaxFileAgeScheduleOffset"
+    property "MaxImportFrequency"
+    property "Name"
+    property "NullRepresentation"
+    property "ObjectID"
+    property "ObjectState"
+    property "PartnerKey"
+    property "RetrieveFileTransferLocation"
+    property "StandardQuotedStrings"
+
+  end
+end
